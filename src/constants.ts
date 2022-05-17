495820a1a47786477dce72489166eef36995f129
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-05-17 15:31:41 -0400

    Make builtin regex more strict
    Previously, we could match on portions of relative paths.
    This ensures that only an entire match of the builtin module names will be considered.

--- src/constants.ts
+++ src/constants.ts
@@ -16,7 +16,7 @@ export const chunkTypeOther = 'other';
  * where the not matched imports should be placed
  */
 export const THIRD_PARTY_MODULES_SPECIAL_WORD = '<THIRD_PARTY_MODULES>';
-export const BUILTIN_MODULES = builtinModules.join('|');
+export const BUILTIN_MODULES = `^(?:${builtinModules.join('|')})$`;
 
 const PRETTIER_PLUGIN_SORT_IMPORTS_NEW_LINE =
     'PRETTIER_PLUGIN_SORT_IMPORTS_NEW_LINE';
