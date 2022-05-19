9ef2abeef44b41dbd4fa3e9acc99e7390eae0dba
Frederic Barthelemy <git@fbartho.com>
2022-05-19 12:57:15 -0700

    switch from toMatchInlineSnapshot to toEqual

--- src/utils/__tests__/merge-nodes-with-matching-flavors.spec.ts
+++ src/utils/__tests__/merge-nodes-with-matching-flavors.spec.ts
@@ -51,34 +51,33 @@ it('should merge duplicate imports within a given chunk', () => {
         directives: [],
     });
 
-    expect(format(formatted, { parser: 'babel' })).toMatchInlineSnapshot(`
-        "import type { A, B } from \\"a\\";
-        import { Junk } from \\"junk-group-1\\";
+    expect(format(formatted, { parser: 'babel' }))
+        .toEqual(`import type { A, B } from "a";
+import { Junk } from "junk-group-1";
 
-        import \\"./side-effects1\\";
+import "./side-effects1";
 
-        // C, E and D will be separated from A, B because side-effects in-between
-        import type { C, E } from \\"a\\";
-        import { D } from \\"a\\";
+// C, E and D will be separated from A, B because side-effects in-between
+import type { C, E } from "a";
+import { D } from "a";
 
-        // prettier-ignore
-        import type { NoMerge1 } from \\"a\\";
-        // prettier-ignore
-        import { NoMerge2 } from \\"a\\";
+// prettier-ignore
+import type { NoMerge1 } from "a";
+// prettier-ignore
+import { NoMerge2 } from "a";
 
-        import { F } from \\"a\\";
-        // F Will be alone because prettier-ignore in-between
-        import { G, H } from \\"b\\";
-        import * as J from \\"c\\";
-        import * as K from \\"c\\";
-        // * as J, * as K can't merge because both Namespaces
-        import { I } from \\"c\\";
-        import { default as Def1, default as Def2 } from \\"d\\";
-        import Foo1 from \\"e\\";
-        import Foo2 from \\"e\\";
-        import { Junk2 } from \\"junk-group-2\\";
-        "
-    `);
+import { F } from "a";
+// F Will be alone because prettier-ignore in-between
+import { G, H } from "b";
+import * as J from "c";
+import * as K from "c";
+// * as J, * as K can't merge because both Namespaces
+import { I } from "c";
+import { default as Def1, default as Def2 } from "d";
+import Foo1 from "e";
+import Foo2 from "e";
+import { Junk2 } from "junk-group-2";
+`);
 });
 it("doesn't merge duplicate imports if option disabled", () => {
     const code = `
@@ -127,36 +126,35 @@ it("doesn't merge duplicate imports if option disabled", () => {
         directives: [],
     });
 
-    expect(format(formatted, { parser: 'babel' })).toMatchInlineSnapshot(`
-        "import type { A } from \\"a\\";
-        import type { B } from \\"a\\";
-        import { Junk } from \\"junk-group-1\\";
+    expect(format(formatted, { parser: 'babel' }))
+        .toEqual(`import type { A } from "a";
+import type { B } from "a";
+import { Junk } from "junk-group-1";
 
-        import \\"./side-effects1\\";
+import "./side-effects1";
 
-        // C, E and D will be separated from A, B because side-effects in-between
-        import type { C } from \\"a\\";
-        import { D } from \\"a\\";
-        import type { E } from \\"a\\";
+// C, E and D will be separated from A, B because side-effects in-between
+import type { C } from "a";
+import { D } from "a";
+import type { E } from "a";
 
-        // prettier-ignore
-        import type { NoMerge1 } from \\"a\\";
-        // prettier-ignore
-        import { NoMerge2 } from \\"a\\";
+// prettier-ignore
+import type { NoMerge1 } from "a";
+// prettier-ignore
+import { NoMerge2 } from "a";
 
-        import { F } from \\"a\\";
-        import { H } from \\"b\\";
-        // F Will be alone because prettier-ignore in-between
-        import { G } from \\"b\\";
-        import * as J from \\"c\\";
-        import * as K from \\"c\\";
-        // * as J, * as K can't merge because both Namespaces
-        import { I } from \\"c\\";
-        import { default as Def2 } from \\"d\\";
-        import { default as Def1 } from \\"d\\";
-        import Foo1 from \\"e\\";
-        import Foo2 from \\"e\\";
-        import { Junk2 } from \\"junk-group-2\\";
-        "
-    `);
+import { F } from "a";
+import { H } from "b";
+// F Will be alone because prettier-ignore in-between
+import { G } from "b";
+import * as J from "c";
+import * as K from "c";
+// * as J, * as K can't merge because both Namespaces
+import { I } from "c";
+import { default as Def2 } from "d";
+import { default as Def1 } from "d";
+import Foo1 from "e";
+import Foo2 from "e";
+import { Junk2 } from "junk-group-2";
+`);
 });
