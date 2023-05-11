c516cf22460dfe9bcfc10e180a08517104ab069e
Frederic Barthelemy <git@fbartho.com>
2023-05-10 18:00:03 -0700

    Fix get-all-comments

--- src/utils/get-all-comments-from-nodes.ts
+++ src/utils/get-all-comments-from-nodes.ts
@@ -5,13 +5,32 @@ import type {
     Statement,
 } from '@babel/types';
 
-export const getAllCommentsFromNodes = (nodes: (Directive | Statement)[]) =>
+import { SomeSpecifier } from '../types';
+
+export const getAllCommentsFromNodes = (
+    nodes: (Directive | Statement | SomeSpecifier)[],
+) =>
     nodes.reduce((acc, node) => {
         if (
             Array.isArray(node.leadingComments) &&
             node.leadingComments.length > 0
         ) {
             acc = [...acc, ...node.leadingComments];
+        }
+        if (
+            Array.isArray(node.innerComments) &&
+            node.innerComments.length > 0
+        ) {
+            acc = [...acc, ...node.innerComments];
+        }
+        if (
+            Array.isArray(node.trailingComments) &&
+            node.trailingComments.length > 0
+        ) {
+            acc = [...acc, ...node.trailingComments];
+        }
+        if (node.type === 'ImportDeclaration') {
+            acc = [...acc, ...getAllCommentsFromNodes(node.specifiers)];
         }
         return acc;
     }, [] as (CommentBlock | CommentLine)[]);
