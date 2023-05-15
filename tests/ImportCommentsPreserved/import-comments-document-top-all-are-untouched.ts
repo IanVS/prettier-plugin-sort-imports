20a00f25e28944f8e6567c43e01c24408d3418cd
Ian VanSchooten <ian.vanschooten@gmail.com>
2023-05-15 13:20:13 -0400

    Add test with bad behavior when combining type and value

--- tests/ImportCommentsPreserved/import-comments-document-top-all-are-untouched.ts
+++ tests/ImportCommentsPreserved/import-comments-document-top-all-are-untouched.ts
@@ -3,6 +3,8 @@
  * This comment is before the first import, and so should never be moved.
  * https://github.com/IanVS/prettier-plugin-sort-imports/issues/81
  */
+
+// second comment
+import type { A } from 'a'; // a
 import { b } from 'b';
-import type { A } from 'a';
 import { a } from 'a';
