116071eeb248a7c1daba54176ec62d6747421a9d
Ian VanSchooten <ian.vanschooten@gmail.com>
2023-05-16 22:08:12 -0400

    Failing tests for comments on runtime code within imports

---
+++ tests/ImportCommentsPreserved/leading-comments-on-declaration.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env ts-node
+
+// Leading comment on someRandomGlobal
+const someRandomGlobal = "stuff";
+
+import * as fs from "fs";
+import * as path from "path";
+import shell from "shelljs";
+
