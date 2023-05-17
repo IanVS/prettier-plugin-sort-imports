0c3be1bef6dc859cc15ad436bee6e4ae055d0774
Ian VanSchooten <ian.vanschooten@gmail.com>
2023-05-17 10:59:39 -0400

    Vue: Use correct parser plugins

--- src/preprocessors/vue.ts
+++ src/preprocessors/vue.ts
@@ -1,6 +1,8 @@
 import type { parse as Parse } from '@vue/compiler-sfc';
 
+import { ImportOrderParserPlugin } from '../../types';
 import { PrettierOptions } from '../types';
+import { hasPlugin } from '../utils/get-experimental-parser-plugins';
 import { preprocessor } from './preprocessor';
 
 export function vuePreprocessor(code: string, options: PrettierOptions) {
@@ -10,18 +12,18 @@ export function vuePreprocessor(code: string, options: PrettierOptions) {
         const { descriptor } = parse(code);
 
         if (descriptor.script) {
-            const { content } = descriptor.script;
-            preprocessedCode = preprocessedCode.replace(
-                content,
-                `\n${preprocessor(content, options)}\n`,
+            preprocessedCode = sortScript(
+                descriptor.script,
+                preprocessedCode,
+                options,
             );
         }
 
         if (descriptor.scriptSetup) {
-            const { content } = descriptor.scriptSetup;
-            preprocessedCode = preprocessedCode.replace(
-                content,
-                `\n${preprocessor(content, options)}\n`,
+            preprocessedCode = sortScript(
+                descriptor.scriptSetup,
+                preprocessedCode,
+                options,
             );
         }
 
@@ -34,4 +36,61 @@ export function vuePreprocessor(code: string, options: PrettierOptions) {
             throw err;
         }
     }
+}
+
+function isTS(lang?: string) {
+    return lang === 'ts' || lang === 'tsx';
+}
+
+/**
+ * Configures correct babel plugins, sorts imports in a script or setupScript,
+ * and replaces that script/setupScript within the original code
+ *
+ * Much of this was adapted from https://github.com/vuejs/vue/blob/49b6bd4264c25ea41408f066a1835f38bf6fe9f1/packages/compiler-sfc/src/compileScript.ts#L118-L134
+ *
+ * @param param0 a script or setupScript
+ * @param code Source code of the file
+ * @param options Prettier options
+ * @returns Original code with sorted imports in the script provided
+ */
+function sortScript(
+    { content, lang }: { content: string; lang?: string },
+    code: string,
+    options: PrettierOptions,
+) {
+    const { importOrderParserPlugins = [] } = options;
+    let pluginClone = [...importOrderParserPlugins];
+    const newPlugins: ImportOrderParserPlugin[] = [];
+
+    if (!isTS(lang) || lang === 'tsx') {
+        newPlugins.push('jsx');
+    } else {
+        // Remove jsx if typescript and not tsx
+        pluginClone = pluginClone.filter((p) => p !== 'jsx');
+    }
+
+    newPlugins.push(...pluginClone);
+
+    if (isTS(lang)) {
+        if (!hasPlugin(newPlugins, 'typescript')) {
+            newPlugins.push('typescript');
+        }
+
+        if (
+            !hasPlugin(newPlugins, 'decorators') &&
+            !hasPlugin(newPlugins, 'decorators-legacy')
+        ) {
+            newPlugins.push('decorators-legacy');
+        }
+    }
+
+    const adjustedOptions = {
+        ...options,
+        importOrderParserPlugins: newPlugins,
+    };
+
+    return code.replace(
+        content,
+        `\n${preprocessor(content, adjustedOptions)}\n`,
+    );
 }
--- src/utils/get-experimental-parser-plugins.ts
+++ src/utils/get-experimental-parser-plugins.ts
@@ -32,3 +32,37 @@ export const getExperimentalParserPlugins = (
         return plugin;
     });
 };
+
+/**
+ * Checks whether a specified plugin is included in importOrderParserPlugins.
+ * More fancy than just a `.includes()` because importOrderParserPlugins can contain plugins with configuration
+ *
+ * @param importOrderParserPlugins array of experimental babel parser plugins
+ * @returns true if the plugin is in the list
+ */
+export const hasPlugin = (
+    importOrderParserPlugins: string[],
+    pluginName: string,
+): boolean => {
+    for (const pluginNameOrJson of importOrderParserPlugins) {
+        const isParserPluginWithOptions = pluginNameOrJson.startsWith('[');
+        let plugin;
+
+        if (isParserPluginWithOptions) {
+            try {
+                plugin = JSON.parse(
+                    pluginNameOrJson,
+                ) as ParserPluginWithOptions;
+            } catch (e) {
+                throw Error(
+                    'Invalid JSON in importOrderParserPlugins: ' +
+                        pluginNameOrJson,
+                );
+            }
+        } else {
+            plugin = pluginNameOrJson as ParserPlugin;
+        }
+        if (plugin === pluginName) return true;
+    }
+    return false;
+};
