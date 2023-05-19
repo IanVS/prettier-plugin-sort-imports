0789e070cb1e85720cc9349f28ae7f5205298536
Frederic Barthelemy <git@fbartho.com>
2023-05-19 11:19:10 -0700

    Inject required groups after leading separator if present (#95)
    Created as a separate PR to review if I'm missing anything
    
    ---------
    
    Co-authored-by: Ian VanSchooten <ian.vanschooten@gmail.com>

--- tests/ImportsSeparatedByEmptyRegex/ppsi.spec.ts
+++ tests/ImportsSeparatedByEmptyRegex/ppsi.spec.ts
@@ -2,9 +2,8 @@ import {run_spec} from '../../test-setup/run_spec';
 
 run_spec(__dirname, ['typescript'], {
     importOrder: [
-        // Never insert a blank-line below the top-of-file comments, so no empty string here.
         // '<BUILTIN_MODULES>',     // This is injected by options normalization
-        // '<THIRD_PARTY_MODULES>', // This is injected by options normalization
+        '<THIRD_PARTY_MODULES>',
         '',
         '^@core/(.*)$',
         '^@server/(.*)',
--- tests/Vue/ppsi.spec.ts
+++ tests/Vue/ppsi.spec.ts
@@ -1,5 +1,15 @@
 import {run_spec} from '../../test-setup/run_spec';
 
 run_spec(__dirname, ["vue"], {
-    importOrder: ['', '^@core/(.*)$', '', '^@server/(.*)', '', '^@ui/(.*)$', '', '^[./]'],
+    importOrder: [
+        '<THIRD_PARTY_MODULES>',
+        '',
+        '^@core/(.*)$',
+        '',
+        '^@server/(.*)',
+        '',
+        '^@ui/(.*)$',
+        '',
+        '^[./]',
+    ],
 });
