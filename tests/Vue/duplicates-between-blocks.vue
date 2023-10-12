13bbbdd8e6e659ca6b4e0b925a955e33fb1239f7
Tanimodori <unknowner2014@gmail.com>
2023-10-12 19:23:09 +0800

    fix: vue preprocessor may replace wrong code (#135)

---
+++ tests/Vue/duplicates-between-blocks.vue
@@ -0,0 +1,51 @@
+<template>
+  <code>
+import z from 'z';
+import threeLevelRelativePath from "../../../threeLevelRelativePath";
+import sameLevelRelativePath from "./sameLevelRelativePath";
+import thirdParty from "third-party";
+import oneLevelRelativePath from "../oneLevelRelativePath";
+import otherthing from "@core/otherthing";
+import { defineComponent } from 'vue'
+function add(a,b) {
+  return a + b;
+}
+
+import z from 'z';
+import threeLevelRelativePath from "../../../threeLevelRelativePath";
+import sameLevelRelativePath from "./sameLevelRelativePath";
+import thirdParty from "third-party";
+import oneLevelRelativePath from "../oneLevelRelativePath";
+import otherthing from "@core/otherthing";
+import { defineComponent } from 'vue'
+function add(a,b) {
+  return a + b;
+}
+  </code>
+</template>
+
+<script>
+import z from 'z';
+import threeLevelRelativePath from "../../../threeLevelRelativePath";
+import sameLevelRelativePath from "./sameLevelRelativePath";
+import thirdParty from "third-party";
+import oneLevelRelativePath from "../oneLevelRelativePath";
+import otherthing from "@core/otherthing";
+import { defineComponent } from 'vue'
+function add(a,b) {
+  return a + b;
+}
+</script>
+
+<script setup>
+import z from 'z';
+import threeLevelRelativePath from "../../../threeLevelRelativePath";
+import sameLevelRelativePath from "./sameLevelRelativePath";
+import thirdParty from "third-party";
+import oneLevelRelativePath from "../oneLevelRelativePath";
+import otherthing from "@core/otherthing";
+import { defineComponent } from 'vue'
+function add(a,b) {
+  return a + b;
+}
+</script>
