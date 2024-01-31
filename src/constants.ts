8f368128bc32606fd21645a4421b24b0bc966781
Isaiah Smith <jordankid93@gmail.com>
2024-01-30 22:58:34 -0800

    fix: support node:test in BUILTIN_MODULES_REGEX_STR

--- src/constants.ts
+++ src/constants.ts
@@ -23,9 +23,9 @@ export const mergeableImportFlavors = [
     importFlavorType,
 ] as const;
 
-export const BUILTIN_MODULES_REGEX_STR = `^(?:node:)?(?:${builtinModules.join(
+export const BUILTIN_MODULES_REGEX_STR = `(^(?:node:)?(?:${builtinModules.join(
     '|',
-)})$`;
+)})$)|(^(?:node:test)$)`; // NOTE: 'test' is not included in `builtinModules` so explicitly check for "node:test"
 
 export const BUILTIN_MODULES_SPECIAL_WORD = '<BUILTIN_MODULES>';
 /**
