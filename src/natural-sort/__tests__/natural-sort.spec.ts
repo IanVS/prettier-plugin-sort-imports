4642fe1f8a5869fda5d580ba105a66e1a3f0cdd0
Ian VanSchooten <ian.vanschooten@gmail.com>
2024-09-27 10:36:46 -0400

    Group test cases

--- src/natural-sort/__tests__/natural-sort.spec.ts
+++ src/natural-sort/__tests__/natural-sort.spec.ts
@@ -1,78 +1,82 @@
-import { expect, test } from 'vitest';
+import { expect, test, describe } from 'vitest';
 
 import { naturalSort, naturalSortCaseSensitive } from '..';
 
-test('should sort normal things alphabetically', () => {
-    expect(
-        ['a', 'h', 'b', 'i', 'c', 'd', 'j', 'e', 'k', 'f', 'g'].sort((a, b) =>
-            naturalSort(a, b),
-        ),
-    ).toEqual(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']);
-});
+describe('naturalSort', () => {
+    test('should sort normal things alphabetically', () => {
+        expect(
+            ['a', 'h', 'b', 'i', 'c', 'd', 'j', 'e', 'k', 'f', 'g'].sort((a, b) =>
+                naturalSort(a, b),
+            ),
+        ).toEqual(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']);
+    });
 
-test('should ignore capitalization differences', () => {
-    expect(
-        ['./ExampleComponent', './ExamplesList', './ExampleWidget'].sort(
-            (a, b) => naturalSort(a, b),
-        ),
-    ).toEqual(['./ExampleComponent', './ExamplesList', './ExampleWidget']);
-});
+    test('should ignore capitalization differences', () => {
+        expect(
+            ['./ExampleComponent', './ExamplesList', './ExampleWidget'].sort(
+                (a, b) => naturalSort(a, b),
+            ),
+        ).toEqual(['./ExampleComponent', './ExamplesList', './ExampleWidget']);
+    });
 
-test('should not ignore capitalization differences', () => {
-    expect(
-        ['./ExampleComponent', './ExamplesList', './ExampleWidget'].sort(
-            (a, b) => naturalSortCaseSensitive(a, b),
-        ),
-    ).toEqual(['./ExampleComponent', './ExampleWidget', './ExamplesList']);
-});
+    test('should sort things numerically', () => {
+        expect(['a2', 'a3', 'a10', 'a1', 'a11', 'a9'].sort(naturalSort)).toEqual([
+            'a1',
+            'a2',
+            'a3',
+            'a9',
+            'a10',
+            'a11',
+        ]);
+    });
+})
 
-test('should sort things numerically', () => {
-    expect(['a2', 'a3', 'a10', 'a1', 'a11', 'a9'].sort(naturalSort)).toEqual([
-        'a1',
-        'a2',
-        'a3',
-        'a9',
-        'a10',
-        'a11',
-    ]);
-});
+describe('naturalSortCaseSensitive', () => {
+    test('should not ignore capitalization differences', () => {
+        expect(
+            ['./ExampleComponent', './ExamplesList', './ExampleWidget'].sort(
+                (a, b) => naturalSortCaseSensitive(a, b),
+            ),
+        ).toEqual(['./ExampleComponent', './ExampleWidget', './ExamplesList']);
+    });
 
-test('should sort numerically and case-sensitively', () => {
-    expect(
-        [
-            'file1',
-            'File10',
-            'AbA',
-            'file10',
-            'files10',
-            'a',
+    test('should sort numerically and case-sensitively', () => {
+        expect(
+            [
+                'file1',
+                'File10',
+                'AbA',
+                'file10',
+                'files10',
+                'a',
+                'Ab',
+                'file20',
+                'file22',
+                'file11',
+                'file2',
+                'File20',
+                'aaa',
+                'AAA',
+                'bBb',
+                'BBB',
+            ].sort(naturalSortCaseSensitive),
+        ).toEqual([
+            'AAA',
             'Ab',
+            'AbA',
+            'BBB',
+            'File10',
+            'File20',
+            'a',
+            'aaa',
+            'bBb',
+            'file1',
+            'file2',
+            'file10',
+            'file11',
             'file20',
             'file22',
-            'file11',
-            'file2',
-            'File20',
-            'aaa',
-            'AAA',
-            'bBb',
-            'BBB',
-        ].sort(naturalSortCaseSensitive),
-    ).toEqual([
-        'AAA',
-        'Ab',
-        'AbA',
-        'BBB',
-        'File10',
-        'File20',
-        'a',
-        'aaa',
-        'bBb',
-        'file1',
-        'file2',
-        'file10',
-        'file11',
-        'file20',
-        'file22',
-        'files10',
-    ]);
+            'files10',
+        ]);
+    });
 });
