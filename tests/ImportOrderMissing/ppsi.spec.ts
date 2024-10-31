06d886e3de2919a515155f868b9e8d7ce4958730
Ian VanSchooten <ian.vanschooten@gmail.com>
2024-10-31 12:40:33 -0400

    Add a hint for how to fix it

--- tests/ImportOrderMissing/ppsi.spec.ts
+++ tests/ImportOrderMissing/ppsi.spec.ts
@@ -3,7 +3,7 @@ import {expectError} from '../../test-setup/run_spec';
 expectError(
     __dirname,
     "typescript",
-    "Could not find a matching group in importOrder for: \"React\" on line 3",
+    /Could not find a matching group in importOrder for: \"React\" on line 3. Did you forget to include \"<TYPES>\"\?$/,
     {
         importOrder: ['^[./]', '<TYPES>^#utils$', '<TYPES>[.]'],
         importOrderParserPlugins: ['typescript'],
