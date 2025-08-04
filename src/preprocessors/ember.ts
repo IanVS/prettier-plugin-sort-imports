82a58f1628bd1962b0375defd0baf68a397c79a4
Ian VanSchooten <ian.vanschooten@gmail.com>
2025-08-04 13:33:42 -0400

    Remove convertAST step

--- src/preprocessors/ember.ts
+++ src/preprocessors/ember.ts
@@ -1,15 +1,4 @@
-import {
-    parse as babelParser,
-    type ParseResult,
-    type ParserOptions,
-} from '@babel/parser';
-import traverse from '@babel/traverse';
-import type {
-    BlockStatement,
-    File,
-    ObjectExpression,
-    StaticBlock,
-} from '@babel/types';
+import { parse as babelParser, type ParserOptions } from '@babel/parser';
 
 import { PrettierOptions } from '../types';
 import { extractASTNodes } from '../utils/extract-ast-nodes';
@@ -112,107 +101,9 @@ export function codeToGlimmerAst(code: string): Template[] {
     return templates;
 }
 
-/**
- * Throws an error if the condition is false.
- *
- * If no condition is provided, will always throw.
- */
-export function assert(
-    message: string,
-    condition?: unknown,
-): asserts condition {
-    if (!condition) {
-        throw new Error(message);
-    }
-}
-
-export const TEMPLATE_TAG_NAME = 'template';
-
-export const TEMPLATE_TAG_OPEN = `<${TEMPLATE_TAG_NAME}>`;
-export const TEMPLATE_TAG_CLOSE = `</${TEMPLATE_TAG_NAME}>`;
-
-/** Converts a node into a GlimmerTemplate node */
-function convertNode(
-    node: BlockStatement | ObjectExpression | StaticBlock,
-    rawTemplate: Template,
-): void {
-    node.innerComments = [];
-
-    node.extra = Object.assign(node.extra ?? {}, {
-        isGlimmerTemplate: true as const,
-        template: rawTemplate,
-    });
-}
-
-/** Traverses the AST and replaces the transformed template parts with other AST */
-function convertAst(ast: ParseResult<File>, templates: Template[]): void {
-    traverse(ast, {
-        enter(path) {
-            const { node } = path;
-
-            switch (node.type) {
-                case 'BlockStatement':
-                case 'ObjectExpression':
-                case 'StaticBlock': {
-                    assert('expected range', node.range);
-                    const [start, end] = node.range;
-
-                    const templateIndex = templates.findIndex((template) => {
-                        const { utf16Range } = template;
-
-                        if (
-                            utf16Range.start === start &&
-                            utf16Range.end === end
-                        ) {
-                            return true;
-                        }
-
-                        return (
-                            node.type === 'ObjectExpression' &&
-                            utf16Range.start === start - 1 &&
-                            utf16Range.end === end + 1
-                        );
-                    });
-
-                    if (templateIndex === -1) {
-                        return null;
-                    }
-
-                    const rawTemplate = templates.splice(templateIndex, 1)[0];
-
-                    if (!rawTemplate) {
-                        throw new Error(
-                            'expected raw template because splice index came from findIndex',
-                        );
-                    }
-
-                    const index =
-                        node.innerComments?.[0] &&
-                        ast.comments?.indexOf(node.innerComments[0]);
-
-                    if (ast.comments && index !== undefined && index >= 0) {
-                        ast.comments.splice(index, 1);
-                    }
-
-                    convertNode(node, rawTemplate);
-                }
-            }
-
-            return null;
-        },
-    });
-
-    if (templates.length > 0) {
-        throw new Error(
-            `failed to process all templates, ${templates.length} remaining`,
-        );
-    }
-}
-
 export function sortCode(
     code: string,
     originalCode: string,
-    templates: Template[],
     options: PrettierOptions,
 ): string {
     const { plugins, ...remainingOptions } =
@@ -220,7 +111,6 @@ export function sortCode(
 
     const parserOptions: ParserOptions = {
         sourceType: 'module',
-        ranges: true,
         attachComment: true,
         errorRecovery: true,
         allowReturnOutsideFunction: true,
@@ -245,8 +135,6 @@ export function sortCode(
         );
         return code;
     }
-
-    convertAst(ast, templates);
 
     const directives = ast.program.directives;
     const interpreter = ast.program.interpreter;
@@ -280,7 +168,7 @@ export function emberPreprocessor(code: string, options: PrettierOptions) {
         code = preprocessTemplateRange(template, code);
     }
 
-    const sorted = sortCode(code, originalCode, templates, options);
+    const sorted = sortCode(code, originalCode, options);
 
     return sorted;
 }
